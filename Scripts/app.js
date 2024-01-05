2291d349e8eb8a36b4daae27d9ac24b276d7351a
Aiden Foo <aidenfoo154@gmail.com>
2024-01-05 11:12:19 -0800

    JavaScript for subscribe form

---
+++ Scripts/app.js
@@ -0,0 +1,42 @@
+import { apiKey } from "./ApiKey.js";
+
+let submitBtn = document.getElementById('submitBtn')
+let subscribeInput = document.getElementById('subscribeInput')
+let subscribedText = document.getElementById('subscribedTxt')
+
+
+submitBtn.addEventListener('click', (e) =>{
+    ApiCall()
+    
+})
+
+const ApiCall = async () =>{
+    const url = `https://mailcheck.p.rapidapi.com/?domain=${subscribeInput.value}`;
+    const options = {
+    	method: 'GET',
+    	headers: {
+    	'X-RapidAPI-Key': `${apiKey}`,
+    	'X-RapidAPI-Host': 'mailcheck.p.rapidapi.com'
+    	}
+    };
+
+    try {
+    	const response = await fetch(url, options);
+    	const result = await response.json();
+        if(result.valid)
+        {
+            submitBtn.remove()
+            subscribeInput.remove()
+            const p = document.createElement('p')
+            const p2 = document.createElement('p')
+            p.textContent = "Thank you for subscribing!"
+            p2.textContent = 'We will now send news to your mailbox'
+            subscribedText.append(p, p2)
+        }
+    } 
+    catch (error) 
+    {
+    	console.error(error);
+    }
+    
+}
